5de817b7fd2e5ded8d8551db1e744e3881ad5994
Kiwi7611 <kiwiliu2001@gmail.com>
2020-04-21 15:18:32 -0400

    Create scriptt.js

---
+++ scriptt.js
@@ -0,0 +1,55 @@
+var app = {
+
+    NYbooks: [],
+
+    initialize: function() {
+        $("#bookbutton").click(
+            function(){
+            var BookSearch = $("#bookname").val();
+                console.log(BookSearch);
+                app.GetBooksData(BookSearch);
+            })
+    },
+        
+
+    inserts: function() {
+        var content = " ";
+        for (var i = 0; i < app.NYbooks.length; i++) {
+            content += '<div class="contentlist">'
+            content += '<h2>' + app.NYbooks[i].book_title + '</h2>';
+            content += '<p style="font-style:italic;">' + app.NYbooks[i].book_author + '</p>';
+            content += '<p>' + app.NYbooks[i].summary + '</p>';
+            content += '</div>'
+        }
+        $('#review').html(content);
+    },
+
+
+  
+    GetBooksData: function(BookSearch) {
+
+        var booksAPI = "https://api.nytimes.com/svc/books/v3/reviews.json?title=" + BookSearch + "&api-key=" 
+        var APIKey= "xWQaJb76GoQU3OF98Cjc8xQtEvc6e2kh"
+        var BooksUrl = booksAPI + APIKey;
+        console.log(BooksUrl);
+
+        $.ajax ({
+           url: BooksUrl,
+           type: 'GET',
+           dataType:'json',
+
+           error: function(err){
+            console.log("Uh oh...");
+            console.log(err);
+            },
+
+            success: function(data){
+            console.log(data);
+            app.NYbooks = data.results;
+            app.inserts();
+            }
+        });
+    }
+};
+
+
